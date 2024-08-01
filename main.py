92ceed1906d2825e66a3e109c0ba3293d07206a4
Hayatokane <cheungkuensum@gmail.com>
2024-08-01 02:26:53 +0100

    We can use native python to handle 90bits

--- main.py
+++ main.py
@@ -1,28 +1,83 @@
-from bit90 import Bit90
+import numpy as np
+
+
+def print_bitboard(bitboard):
+    bits = np.binary_repr(bitboard, 90)
+    rows = []
+    for i in range(0, 90, 9):
+        rows.append(bits[i:i + 9])
+    formatted_output = '\n'.join(rows)
+
+    # Add file and rank labels
+    files = "abcdefghi"
+    ranks = "9876543210"
+
+    output_with_labels = "  " + files + "\n"
+    for i, row in enumerate(formatted_output.split('\n')):
+        output_with_labels += ranks[i] + " " + row + "\n"
+    output_with_labels += "  " + files + "\n"
+
+    print(output_with_labels)
 
 
 class Board:
     def __init__(self):
+        self.all_positions = 0x3FE00415540000AAA0801FF
+        self.red_pawns = 0xAA8000000
+        self.black_pawns = 0x5540000000000000
+        self.red_cannons = 0x2080000
+        self.black_cannons = 0x41
+        self.red_rooks = 0x101
+        self.red_horses = 0x82
+        self.red_elephants = 0x44
+        self.red_advisors = 0x28
+        self.red_king = 0x10
+        self.black_rooks = 0x2020000
+        self.black_horses = 0x1040000
+        self.black_elephants = 0x880000
+        self.black_advisors = 0x500000
+        self.black_king = 0x200000
+        self.mask = [0b1 << i for i in range(90)]
+        self.red_pawns_actions = [0b0 for _ in range(90)]
+        self.black_pawns_actions = [0b0 for _ in range(90)]
+        self.generate_pawns_actions()
 
-        self.all_positions = Bit90(0x5540000AAA0801FF, 0x3FE0041)
-        self.red_pawns = Bit90(0xAA8000000)
-        self.black_pawns = Bit90(0x5540000000000000)
-        self.red_cannons = Bit90(0x2080000)
-        self.black_cannons = Bit90(high=0x41)
-        self.red_rooks = Bit90(0x101)
-        self.red_knights = Bit90(0x82)
-        self.red_bishops = Bit90(0x44)
-        self.red_advisors = Bit90(0x28)
-        self.red_king = Bit90(0x10)
-        self.black_rooks = Bit90(high=0x2020000)
-        self.black_knights = Bit90(high=0x1040000)
-        self.black_bishops = Bit90(high=0x880000)
-        self.black_advisors = Bit90(high=0x500000)
-        self.black_king = Bit90(high=0x200000)
-        self.mask = [Bit90() for _ in range(91)]
-        for i in range(91):
-            self.mask[i].set_bit(i)
-        self.knight_moves = self.generate_knight_moves()
+    def generate_pawns_actions(self):
+        # Generate actions for red pawns
+        for i in range(27, 90):
+            if i <= 44:
+                # Only move forward
+                if i + 9 < 90:
+                    self.red_pawns_actions[i] = self.mask[i + 9]
+            else:
+                # Move forward and sideways
+                actions = 0
+                if i + 9 < 90:
+                    actions |= self.mask[i + 9]
+                if (i % 9) > 0:  # Can move left
+                    actions |= self.mask[i - 1]
+                if (i % 9) < 8:  # Can move right
+                    actions |= self.mask[i + 1]
+                self.red_pawns_actions[i] = actions
+
+        # Generate actions for black pawns
+        for i in range(62, -1, -1):
+            if i >= 45:
+                # Only move forward
+                if i - 9 >= 0:
+                    self.black_pawns_actions[i] = self.mask[i - 9]
+            else:
+                # Move forward and sideways
+                actions = 0
+                if i - 9 >= 0:
+                    actions |= self.mask[i - 9]
+                if (i % 9) > 0:  # Can move left
+                    actions |= self.mask[i - 1]
+                if (i % 9) < 8:  # Can move right
+                    actions |= self.mask[i + 1]
+                self.black_pawns_actions[i] = actions
+
+    def
 
 
 board = Board()
